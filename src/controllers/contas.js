a5b791d56c091545a655f98b0f14ea46fac94ba1
Murilo M Andrade <andrade.murilom@gmail.com>
2023-06-11 22:59:45 -0300

    Endpoint de listagem de contas finalizado com validação de senha administrativa

--- src/controllers/contas.js
+++ src/controllers/contas.js
@@ -1,13 +1,14 @@
-const funcAux = require('../utils/func-aux');
 const fs = require('fs/promises')
 
 
 const listarContas = async (req, res) => {
-    const {senha_banco} = req.params;
-    
-    const banco = JSON.parse(await fs.readFile('./src/database/banco.json'));
-    
-    console.log(banco);
+    const { senha_banco } = req.params;
+    const dadosBanco = JSON.parse(await fs.readFile('./src/database/banco.json'));
+
+    if (senha_banco !== dadosBanco.banco.senha) {
+        return res.status(400).json({ mensagem: "Senha incorreta" })
+    }
+    return res.status(200).json(dadosBanco.contas)
 }
 
-module.exports = {listarContas}
+module.exports = { listarContas }
