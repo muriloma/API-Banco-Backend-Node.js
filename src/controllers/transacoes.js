ea49dfd9fa39c31d0061a57a3c19c074f214d488
Murilo M Andrade <andrade.murilom@gmail.com>
2023-07-09 23:10:24 -0300

    Criado o endpoint de depósito

--- src/controllers/transacoes.js
+++ src/controllers/transacoes.js
@@ -1,11 +1,12 @@
 const fs = require('fs/promises');
 const aux = require('../utils/auxfunc');
+const { format } = require('date-fns');
 
 // Você deverá, OBRIGATORIAMENTE:
 
 // Verificar se o numero da conta e o valor do deposito foram informados no body OK
 // Verificar se a conta bancária informada existe OK
-// Não permitir depósitos com valores negativos ou zerados
+// Não permitir depósitos com valores negativos ou zerados ok
 // Somar o valor de depósito ao saldo da conta encontrada
 
 const depositar = async (req, res) => {
@@ -13,28 +14,41 @@ const depositar = async (req, res) => {
 
     // O numero da conta vem como string e o valor como number
     if (!numeroConta || !valor) {
-        return res.status(400).json({ mensagem: 'Por favor informar dados válidos' })
-    }
+        return res.status(400).json({ mensagem: 'Por favor informar dados válidos' });
+    };
 
     try {
         const dadosBanco = JSON.parse(await fs.readFile('./src/database/banco.json'));
 
-        const conta = await aux.buscarConta(numeroConta);
+        const conta = dadosBanco.contas.find((conta) => {
+            return numeroConta === conta.numero
+        });
+
         if (!conta) {
-            return res.status(404).json({ mensagem: "Conta não localizada" })
+            return res.status(404).json({ mensagem: "Conta não localizada" });
         };
 
         if (Math.sign(valor) !== 1 || valor === 0) {
-            return res.status(400).json({ mensagem: 'Valor não permitido para essa transação' })
-        }
+            return res.status(400).json({ mensagem: 'Valor não permitido para essa transação' });
+        };
 
+        conta.saldo += valor
+        const data = format(new Date(), "dd-MM-yyyy' 'HH:mm:ss");
 
+        const deposito = {
+            data,
+            numero_conta: String(numeroConta),
+            valor
+        };
 
+        dadosBanco.depositos.push(deposito);
+        await fs.writeFile('./src/database/banco.json', JSON.stringify(dadosBanco));
 
+        return res.status(201).json({ mensagem: "Depósito realizado com sucesso!" });
 
     } catch (erro) {
         return res.status(500).json({ Erro: erro.message });
-    }
+    };
 
 };
 
